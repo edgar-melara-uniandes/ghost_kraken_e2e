1a8a871ca8443421c67971faee23ff6716b43b93
edgar-melara-uniandes <e.melara@uniandes.edu.co>
2022-05-07 17:42:23 -0600

    feat: agregar pageObjects

---
+++ features/web/pageObjects/home.page.js
@@ -0,0 +1,43 @@
+module.exports = class HomePage {
+
+    constructor(driver) {
+        this.driver = driver;
+    }
+
+    get elePostsLink() {
+        const postsLink = this.driver.$("text='Posts'");
+        if(postsLink != null) {
+            return postsLink;
+        } else {
+            throw new Error("No postsLink element");
+        }
+    }
+
+    get elePagesLink() {
+        const pagesLink = this.driver.$("text='Pages'");
+        if(pagesLink != null) {
+            return pagesLink;
+        } else {
+            throw new Error("No pagesLink element");
+        }
+    }
+
+    get eleTagsLink() {
+        const tagsLink = this.page.$("text='Tags'");
+        if(tagsLink != null) {
+            return tagsLink;
+        } else {
+            throw new Error("No tagsLink element");
+        }
+    }
+
+    get eleStaffLink() {
+        const staffLink = this.page.$("text='Staff'");
+        if(staffLink != null) {
+            return staffLink;
+        } else {
+            throw new Error("No staffLink element");
+        }
+    }
+
+}
--- features/web/pageObjects/post.page.js
+++ features/web/pageObjects/post.page.js
@@ -1,17 +1,16 @@
-module.exports = class Page {
-    
+module.exports =  class PostPage {
+
     constructor(driver) {
         this.driver = driver;
     }
 
-   get eleNewPageLink() {
-        const newPostLink = this.driver.$("text='New page'");
+    get eleNewPostLink() {
+        const newPostLink = this.driver.$("text='New post'");
         if(newPostLink != null) {
             return newPostLink;
         } else {
             throw new Error("No newPostLink element");
         }
     }
-
 
 }
